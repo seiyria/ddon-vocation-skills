465ba1213cba0163481e27dd32837c374f904f63
Kyle Kemp <kyle@seiyria.com>
2017-11-19 15:33:29 -0600

    add first skill iteration - sorcerer

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# DDON Vocation Skills
+
+A handy tool to sort through the massive amount of vocation information needed to understand how to play it.
+
+## Credits
+
+* Guren (and the [DDON Skills Spreadsheet](https://docs.google.com/spreadsheets/d/1MF7PXo1uZnXQyTMNuMUsvRqyf1JiQgikq1f2glR5BWU/edit#gid=1927142389))
---
+++ index.js
@@ -0,0 +1,75 @@
+
+var vue = new Vue({
+    el: '#app',
+    data: {
+        selectedVocation: '',
+        allVocations: [
+            { name: 'Fighter',          color: 'danger' },
+            { name: 'Hunter',           color: 'danger' },
+            { name: 'Priest',           color: 'warning' },
+            { name: 'Shield Sage',      color: 'info' },
+            { name: 'Seeker',           color: 'danger' },
+            { name: 'Sorcerer',         color: 'danger' },
+            { name: 'Element Archer',   color: 'warning' },
+            { name: 'Warrior',          color: 'danger' },
+            { name: 'Alchemist',        color: 'info' },
+            { name: 'Spirit Lancer',    color: 'warning' },
+        ],
+        showData: { baseSkills: true, unlockedSkills: true, baseAugs: true, unlockedAugs: true },
+        vocationData: {},
+        neededData: [],
+        loading: true
+    },
+    methods: {
+        selectVocation(vocation, ignoreHashUpdate) {
+            this.selectedVocation = vocation;
+            if(!ignoreHashUpdate) {
+                updateHash();
+            }
+        },
+        selectSkill(skillName) {
+            updateHash(skillName);
+        }
+    }
+});
+
+function loadFromHash() {
+    if(!window.location.hash) return;
+
+    var baseHash = decodeURIComponent(window.location.hash.substring(1));
+
+    if(baseHash.indexOf('-') !== -1) {
+
+        var vocation = baseHash.split('-')[0];
+        if(!vue.vocationData[vocation]) return;
+
+        vue.selectedVocation = vocation;
+
+        Vue.nextTick(() => {
+            document.getElementById(baseHash).scrollIntoView();
+        });
+
+    } else {
+
+        if(!vue.vocationData[baseHash]) return;
+
+        vue.selectedVocation = baseHash;
+    }
+
+}
+
+function updateHash(skillName) {
+    window.location.hash = encodeURIComponent(vue.selectedVocation + (skillName ? '-' + skillName : ''));
+}
+
+function formatData(data) {
+    return data;
+}
+
+axios.get('skills.yml')
+    .then(res => {
+        vue.vocationData = formatData(YAML.parse(res.data));
+        vue.loading = false;
+
+        loadFromHash();
+    });
